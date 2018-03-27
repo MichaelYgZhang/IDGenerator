6f0239a98d38a9ed24be753363aaefe98f359ccb
michaelygzhang <g.zhangyg@gmail.com>
2018-03-27 23:27:04 +0800

    init

---
+++ src/main/java/com/home/Hello.java
@@ -0,0 +1,13 @@
+package com.home;
+
+/**
+ * @Description
+ * @Author michael
+ * @DateTime 2018/3/27 23:22
+ * @Version 1.0.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
